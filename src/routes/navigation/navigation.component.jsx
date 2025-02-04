d7e8686d8b15e75b59588091a1e380bb829f4fc2
sotreduch2602 <sotreduch26022001.com>
2025-02-04 12:08:30 +0700

    update CrwnLogo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,22 @@
-import { Outlet } from "react-router-dom";
-
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
 const Navigation = () => {
   return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to={"/"}>
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to={"/shop"}>
+            SHOP
+          </Link>
+        </div>
       </div>
       <Outlet />
-    </div>
+    </Fragment>
   );
 };
 
