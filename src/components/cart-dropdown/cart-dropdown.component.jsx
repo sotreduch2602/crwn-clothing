c7621a98633a56c047f5837e93462aeea9e78728
sotreduch2602 <sotreduch26022001@gmail.com>
2025-03-13 21:28:14 +0700

    Thêm giao diện cho CartDropdown: cập nhật cấu trúc JSX, thêm nút "GO TO CHECKOUT" và định nghĩa các kiểu CSS cho thành phần giỏ hàng.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,12 @@
+import Button from "../button/button.component";
 import "./cart-dropdown.styles.scss";
 const CartDropdown = () => {
-  return <div className="cart-dropdown-container"></div>;
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items"></div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
 };
 
 export default CartDropdown;
