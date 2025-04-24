a9104c6f7b6dc54adda987407f49bab516560b79
sotreduch2602 <sotreduch26022001@gmail.com>
2025-04-24 12:52:51 +0700

    Refactor Checkout component: streamline code by removing unnecessary elements and ensure proper integration with CartContext for item management.

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import { useContext } from "react";
-import "./checkout.styles.scss";
-import { CartContext } from "../../contexts/cart.context";
-
-const Checkout = () => {
-  const { cartItems, addItemToCart } = useContext(CartContext);
-
-  return (
-    <div>
-      <h1>I am the checkout page</h1>
-      <div>
-        {cartItems.map((cartItem) => {
-          const { id, name, quantity } = cartItem;
-          return (
-            <div key={id}>
-              <h2>{name}</h2>
-              <span>{quantity}</span>
-              <br />
-              <span>decrement</span>
-              <br />
-              <span onClick={() => addItemToCart(cartItem)}>increment</span>
-            </div>
-          );
-        })}
-      </div>
-=======
 import { CartContext } from "../../contexts/cart.context";
 import { useContext } from "react";
 import "./checkout.styles.scss";
@@ -59,7 +32,6 @@ const Checkout = () => {
         return <CheckoutItem key={id} cartItem={cartItem} />;
       })}
       <span className="total">Total: 0</span>
->>>>>>> 6a31f9388aca4fd0773cf40bbdfb65a0f49dde49
     </div>
   );
 };
