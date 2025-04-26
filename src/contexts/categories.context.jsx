56719e2bc90c1f0363d2a66100c662c88b5d5a36
sotreduch2602 <sotreduch26022001.com>
2025-04-26 21:15:27 +0700

    update nested route of category

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -16,7 +16,7 @@ export const CategoriesProvider = ({ children }) => {
       setCategoriesMap(categoryMap);
     };
     getCategoriesMap();
-  });
+  }, []);
 
   const value = { categoriesMap };
 
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,18 +1,15 @@
-import { useContext } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
+import { Routes, Route } from "react-router-dom";
+
 import "./shop.styles.scss";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
+
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />}></Route>
+      <Route path=":category" element={<Category />}></Route>
+    </Routes>
   );
 };
 
