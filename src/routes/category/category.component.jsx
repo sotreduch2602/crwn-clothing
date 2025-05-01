a16dc48fe886335d1357fe229dc6afa50ee1516c
sotreduch2602 <sotreduch26022001.com>
2025-05-01 19:59:07 +0700

    Fix Clashing Styles

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { CategoriesContext } from "../../contexts/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
 
-import './category.styles.scss'
+import "./category.styles.scss";
 
 const Category = () => {
   const { category } = useParams();
@@ -15,12 +15,15 @@ const Category = () => {
   }, [category, categoriesMap]);
 
   return (
-    <div className="category-container">
-      {products &&
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
+    <Fragment>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </Fragment>
   );
 };
 
