d75f577c8d6f81e05da022a0eca564b17da93a04
sotreduch2602 <sotreduch26022001.com>
2025-05-02 17:28:58 +0700

    Styled-Component - Cart Dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,7 +2,11 @@ import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
-import "./cart-dropdown.styles.jsx";
+import {
+  CartDropdownContainer,
+  EmptyMessage,
+  CartItems,
+} from "./cart-dropdown.styles";
 import { useNavigate } from "react-router-dom";
 
 const CartDropdown = () => {
@@ -14,16 +18,16 @@ const CartDropdown = () => {
   };
 
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
+    <CartDropdownContainer>
+      <CartItems>
         {cartItems.length ? (
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
         ) : (
-          <span>Your cart is empty</span>
+          <EmptyMessage>Your cart is empty</EmptyMessage>
         )}
-      </div>
+      </CartItems>
       <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-    </div>
+    </CartDropdownContainer>
   );
 };
 
--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+} from "../button/button.styles";
 
 export const CartDropdownContainer = styled.div`
   position: absolute;
@@ -12,30 +17,22 @@ export const CartDropdownContainer = styled.div`
   top: 90px;
   right: 40px;
   z-index: 5;
-`
-  
-export const EmptyMessage = styled.span`
-    font-size: 18px;
-    margin: 50px auto;
-  `
 
-export const CartItems = styled.div`
-    height: 240px;
-    display: flex;
-    flex-direction: column;
-    overflow: scroll;
-  `
-
-.cart - dropdown - container {
-  .empty-message {
-
-  }
-
-  .cart-items {
-
-  }
-
-  button {
+  ${BaseButton},
+  ${GoogleSignInButton},
+  ${InvertedButton} {
     margin-top: auto;
   }
-}
+`;
+
+export const EmptyMessage = styled.span`
+  font-size: 18px;
+  margin: 50px auto;
+`;
+
+export const CartItems = styled.div`
+  height: 240px;
+  display: flex;
+  flex-direction: column;
+  overflow: scroll;
+`;
