33a3a28f06a4cbbabe7844249e1a661c82d8ea60
sotreduch2602 <sotreduch26022001.com>
2025-05-08 15:02:20 +0700

    Styled-Component - Directory Item & Cart Icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,4 @@
-import "./cart-icon.styles.scss";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import {ShoppingIcon,CartIconContainer,ItemCount} from "./cart-icon.styles";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 const CartIcon = () => {
@@ -10,10 +9,10 @@ const CartIcon = () => {
 
   const { cartCount } = useContext(CartContext);
   return (
-    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
+    <CartIconContainer className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartCount}</span>
-    </div>
+      <ItemCount className="item-count">{cartCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
