91dc24e6bb97a7b19251de30dc30dc8a3d978bff
sotreduch2602 <sotreduch26022001.com>
2025-05-15 21:22:26 +0700

    151.Redux vs Context:Access

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,20 +1,23 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { CartContext } from "../../contexts/cart.context";
+
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
+
 import {
   CartDropdownContainer,
   EmptyMessage,
   CartItems,
 } from "./cart-dropdown.styles";
-import { useNavigate } from "react-router-dom";
 
 const CartDropdown = () => {
-  const { cartItems } = useContext(CartContext);
-  const naviagte = useNavigate();
+  const { cartItems } = useContext(CartContext);  
+  const navigate = useNavigate();
 
   const goToCheckoutHandler = () => {
-    naviagte("/checkout");
+    navigate("/checkout");
   };
 
   return (
